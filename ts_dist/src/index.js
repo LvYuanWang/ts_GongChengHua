c0a7fe552cdc9daab27ad3506fc036d3cb890373
LvYuanWang <1175430934@qq.com>
2024-09-07 23:42:40 +0800

    feat: 模块化路径别名及模块解释最佳实践

---
+++ ts_dist/src/index.js
@@ -0,0 +1,9 @@
+// ESModule 的模块化
+import { show } from "./myModule.js";
+import { moduleMethod } from "./Module/type.js";
+import { fn } from "a";
+import pak from "../package.json" assert { type: "json" }; // assert { type: "json" } 用于告诉 esbuild 这是一个 json 文件，不要将其视为 js 文件
+show();
+moduleMethod();
+fn();
+console.log(pak);
